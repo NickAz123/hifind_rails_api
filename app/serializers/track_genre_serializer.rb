9020c8ad8fd88b1695eedb6599aa7774260fa41a
Nick <nick.azurin@gmail.com>
2022-05-23 16:52:13 -0700

    removed some comments

--- app/serializers/track_genre_serializer.rb
+++ app/serializers/track_genre_serializer.rb
@@ -1,4 +1,3 @@
 class TrackGenreSerializer < ActiveModel::Serializer
   attributes :genre_id
-  # has_one :genre, foreign_key: 'id'
 end
--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,6 +1,6 @@
 class TrackSerializer < ActiveModel::Serializer
   attributes :id, :name, :artist, :producers, :mixengineer, :masterengineer, :releasedate, :imagesrc
-  # has_many :track_genres
+  
   has_many :genres
   has_many :elements
 end
